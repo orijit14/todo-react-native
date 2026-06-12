c9af75f08141e40740efb7307cc65e9ee778197a
orijit14 <orijit14@gmail.com>
2026-06-12 18:56:28 +0530

    Update floating add button

--- src/components/FloatingAddButton.jsx
+++ src/components/FloatingAddButton.jsx
@@ -1,30 +1,43 @@
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
-import { Pressable, StyleSheet } from "react-native";
-
+import { Pressable, StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 const FloatingAddButton = () => {
+  const insets = useSafeAreaInsets();
   return (
-    <Pressable style={styles.button} onPress={() => router.push("/add-task")}>
-      <MaterialCommunityIcons name="plus" color="#fff" size={40} />
-    </Pressable>
+    <View style={[styles.glow, { bottom: insets.bottom + 80 }]}>
+      <Pressable style={styles.button} onPress={() => router.push("/add-task")}>
+        <MaterialCommunityIcons name="plus" color="#fff" size={40} />
+      </Pressable>
+    </View>
   );
 };
 
 export default FloatingAddButton;
 
 const styles = StyleSheet.create({
+  glow: {
+    position: "absolute",
+    backgroundColor: "rgba(103, 90, 235, 0.12)",
+    alignSelf: "center",
+    /* bottom: 115, */
+    width: 90,
+    height: 90,
+    borderRadius: 45,
+    justifyContent: "center",
+    alignItems: "center",
+  },
   button: {
     position: "absolute",
     backgroundColor: "#675AEB",
-    bottom: 60,
     borderRadius: 35,
     alignSelf: "center",
     width: 70,
     height: 70,
     justifyContent: "center",
     alignItems: "center",
-    zIndex: 999,
-    elevation: 15,
+    // zIndex: 999,
+    elevation: 12,
     shadowColor: "#675AEB",
     shadowOffset: {
       width: 0,
